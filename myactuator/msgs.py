869b2e48461fa6868a906712b2eb19ffd183eea2
nate-adkins <nathanpadkins@gmail.com>
2024-10-30 14:24:30 -0400

    small typo

--- myactuator/msgs.py
+++ myactuator/msgs.py
@@ -141,7 +141,7 @@ class ReadMotorStatus1Msg(_BaseMsg):
     _sent_parameters = []
     _received_parameters = [
         _CanMsgParam('motor_temperature_c', 1, 1, lambda x: x),
-        _CanMsgParam('MOS_temperature', 2, 1, lambda x: x),
+        _CanMsgParam('mos_temperature', 2, 1, lambda x: x),
         _CanMsgParam('break_state', 3, 1, lambda x: x),
         _CanMsgParam('voltage_volts', 4, 2, lambda x: x),
         _CanMsgParam('error_status', 6, 2, lambda x: x),
